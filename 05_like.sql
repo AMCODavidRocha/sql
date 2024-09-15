b48f8b5e6fcc7d0682c9bd846824774bcdafd102
DavidRocha <davidll@amcoonline.net>
2024-09-15 13:49:23 -0600

    SQL LIKE y AND OR NOT

---
+++ 05_like.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE email LIKE '%yahoo.com'
+
+SELECT * FROM users WHERE email LIKE 'ana%';
+
+SELECT * FROM users WHERE email LIKE '%@%';
---
+++ 06_and_or_not.sql
@@ -0,0 +1,5 @@
+select * from users where not email = 'blodoffer@yahoo.com'
+
+select * from users where not email = 'blodoffer@yahoo.com' and age = 7
+
+select * from users where not email = 'ana@ana.com' or age = 33
