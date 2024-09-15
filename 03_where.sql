b5c3b020f264b69338ba1453ca21e51b21e1d8bd
DavidRocha <davidll@amcoonline.net>
2024-09-15 13:18:31 -0600

    SQL WHERE y ORDER BY

---
+++ 03_where.sql
@@ -0,0 +1,5 @@
+select * from users where age = 32
+
+SELECT name FROM users WHERE age = 32
+
+SELECT DISTINCT age FROM users WHERE age = 32
---
+++ 04_order_by.sql
@@ -0,0 +1,7 @@
+SELECT * FROM users ORDER BY age 
+
+SELECT * FROM users ORDER BY age DESC
+
+SELECT * FROM users WHERE email='ana@ana.com' ORDER BY user_id DESC
+
+SELECT name FROM users WHERE email='ana@ana.com' ORDER BY age DESC
