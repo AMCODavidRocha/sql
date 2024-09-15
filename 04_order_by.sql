5f5bdc75af668e9ef88adfdcd655624c5ac10baa
DavidRocha <davidll@amcoonline.net>
2024-09-15 15:00:07 -0600

    SQL COMMENTS y se agregan comentarios al resto de archivos

--- 04_order_by.sql
+++ 04_order_by.sql
@@ -1,7 +1,11 @@
+-- Ordena todos los datos de la tabla "users" por edad (ascendente por defecto)
 SELECT * FROM users ORDER BY age 
 
+-- Ordena todos los datos de la tabla "users" por edad de manera descendente
 SELECT * FROM users ORDER BY age DESC
 
+-- Obtiene todos los datos de la tabla "users" con email igual a ana@ana.com y los ordena por edad de manera descendente
 SELECT * FROM users WHERE email='ana@ana.com' ORDER BY user_id DESC
 
+-- Obtiene todos los nombres de la tabla "users" con email igual a ana@ana.com y los ordena por edad de manera descendente
 SELECT name FROM users WHERE email='ana@ana.com' ORDER BY age DESC
--- 07_limit.sql
+++ 07_limit.sql
@@ -1,1 +1,5 @@
+-- Obtiene las 3 primeras filas de la tabla "users"
 select * from users limit 3
+
+-- Obtiene las 2 primeras filas de la tabla "users" con email distinto a sara@gmail.com o edad igual a 15
+SELECT * FROM users WHERE NOT email = 'ana@ana.com' OR age = 33 LIMIT 2;
